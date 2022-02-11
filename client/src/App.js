e0b12eaf411173b82051981530007430b4506ff1
julio <julior0518@gmail.com>
2022-02-10 22:54:16 -0500

    setup routerDom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import axios from 'axios';
+import { Route, Switch} from 'react-router-dom'
+import Nav from './components/Nav';
+import LogInOut from './components/LogInOut';
+import Movies from './components/Movie';
 
 function App() {
   return (
     <div className="App">
-    
+      < Nav />
+
+      <Switch>
+        <Route exact path="/" render={Movies} />
+        <Route exact path="/logInOut" render={LogInOut} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+return (
+    <div className="Nav">
+    <p>Nav</p>
+    </div>
+);
+}
+
+export default Nav;
